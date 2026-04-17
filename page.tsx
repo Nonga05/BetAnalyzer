ffedf16dce3b71035ccaad70b9f0b840115c1bc7
Nonga05 <kenedanderson@hotmail.com>
2026-04-17 18:09:53 -0400

    Delete page.tsx

--- page.tsx
+++
@@ -1,7 +0,0 @@
-import ClientApp from '@/components/ClientApp'
-import { getDashboardData } from '@/lib/api-football'
-
-export default async function Page() {
-  const data = await getDashboardData()
-  return <ClientApp initialData={data} />
-}
